80128344218cfd3f0f2ea07397d6eeb712abda38
minsubkim <kpukms21@naver.com>
2019-05-15 16:39:45 +0900

    2차 수정
    2차 수정

--- termProject.py
+++ termProject.py
@@ -2,19 +2,20 @@ from xml.dom.minidom import *
 import urllib.request
 import base64
 from tkinter import *
+import tkinter.messagebox
 from tkinter import font
 import tkinter.messagebox
 import smtplib
+
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
-#import spam
 
 g_Tk = Tk()
-g_Tk.geometry("400x500+750+200")
+g_Tk.geometry("400x550+750+200")
 DataList = []
 photo = PhotoImage(file="0.gif")
 
-myServerKey = "YjP0A7ghBcMdP5gZKDPuAL0FW03YzkK9jsDUXlBIemyfV5jx6A9mA8MKJ8HgI8EKLT4sxCxFKD%2Bq0CcFQg5rhw%3D%3D"
+myServerKey = "X7No6HCPBgJhYags6pqGcwZKpPRvptzqilQ0zoKuP%2FO1g%2FBqeSKmbmtNftmwCzcKEtRssCQh24ruD%2FP8yJG16Q%3D%3D"
 #myLocationBoxData = ['서울','부산','대구','인천','광주','대전','울산','경기','강원','충북','충남','전북','경북','경남','제주','세종']
 myLocationBoxData = "0"
 
@@ -22,7 +23,7 @@ def LoadXML():
     #serverurl = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?ServiceKey="
     serverurl = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?serviceKey="
     #servervalue = "&sidoName=" + urlencode(self.LocationBoxData) + "&numOfRows=999&pageSize=999&pageNo=1&startPage=1"
-    servervalue = "&numOfRows=999&pageNo=1&sidoName=" + urlencode(myLocationBoxData) + "&searchCondition=HOUR"
+    servervalue = "&numOfRows=999&pageSize=999&pageNo=1&startPage=1&sidoName=" + urlencode(myLocationBoxData) + "&searchCondition=HOUR"
     areaData = openAPItoXML(serverurl, myServerKey, servervalue)
     #t1 = print(getPasingData(areaData,"item"))
 
@@ -63,30 +64,47 @@ def getPasingData(xmlData, motherData):
 
     for index in range(citySize):
         mphms = cityList[index].getElementsByTagName("dataTime")
-        list.append(str("시간기준 : "+ mphms[0].firstChild.data))
-        Contentdata += str("시간기준 : "+ mphms[0].firstChild.data) + str("\n")
+        list.append(str("[시간기준] : "+ mphms[0].firstChild.data))
+        Contentdata += str("[시간기준] : "+ mphms[0].firstChild.data) + str("\n")
 
         mphms = cityList[index].getElementsByTagName("cityName")
-        list.append(str("시/군/구 : " + mphms[0].firstChild.data))
-        Contentdata += str("시/군/구 : " + mphms[0].firstChild.data) + str("\n")
+        list.append(str("[시/군/구] : " + mphms[0].firstChild.data))
+        Contentdata += str("[시/군/구] : " + mphms[0].firstChild.data) + str("\n\n")
+
+        mphms = cityList[index].getElementsByTagName("so2Value")
+        list.append(str("[이황산가스] : " + mphms[0].firstChild.data + "ppm " ))
+        Contentdata += str("[이황산가스] : " + mphms[0].firstChild.data + "ppm ") + str("\n")
+
+        mphms = cityList[index].getElementsByTagName("o3Value")
+        list.append(str("[오존] : " + mphms[0].firstChild.data + "ppm "))
+        Contentdata += str("[오존] : " + mphms[0].firstChild.data + "ppm ") + str("\n")
+
+        mphms = cityList[index].getElementsByTagName("no2Value")
+        list.append(str("[이산화질소] : " + mphms[0].firstChild.data + "ppm "))
+        Contentdata += str("[이산화질소] : " + mphms[0].firstChild.data + "ppm ") + str("\n")
+
+        mphms = cityList[index].getElementsByTagName("coValue")
+        list.append(str("[일산화탄소] : " + mphms[0].firstChild.data + "ppm "))
+        Contentdata += str("[일산화탄소] : " + mphms[0].firstChild.data + "ppm ") + str("\n")
 
         mphms = cityList[index].getElementsByTagName("pm10Value")
-        list.append(str("PM10 : " + mphms[0].firstChild.data) + "㎍/m³")
-        Contentdata += str("PM10 : " + mphms[0].firstChild.data + "㎍/m³") + str("\n")
+        if mphms[0].firstChild.data == '-':
+            mphms[0].firstChild.data = '0'
+        list.append(str("[PM10 미세먼지] : " + mphms[0].firstChild.data) + "㎍/m³")
+        Contentdata += str("[PM10 미세먼지] : " + mphms[0].firstChild.data + "㎍/m³") + str("\n")
+
         pm10sum += int(mphms[0].firstChild.data)
 
-
         mphms = cityList[index].getElementsByTagName("pm25Value")
-        list.append(str("PM2.5 : " + mphms[0].firstChild.data + "㎍/m³" + "\n"))
-        Contentdata += str("PM2.5 : " + mphms[0].firstChild.data + "㎍/m³") + str("\n\n")
-        #pm25sum += int(mphms[0].firstChild.data)
+        if mphms[0].firstChild.data == '-':
+            mphms[0].firstChild.data = '0'
+        list.append(str("[PM2.5 미세먼지] : " + mphms[0].firstChild.data +"㎍/m³"+ "\n"))
+        Contentdata += str("[PM2.5 미세먼지] : " + mphms[0].firstChild.data + "㎍/m³") + str("\n\n\n")
+        pm25sum += int(mphms[0].firstChild.data)
 
-        pmcnt+=1
-
-    Image10(pm10sum, pmcnt)
-
+    Image10(pm10sum, index)
+    Image25(pm25sum, index)
     return list
-
 def addParsingDicList(xmlData, motherData, childData):
     # 파싱된 데이터를 리스트에 넣어서 리턴 한다.
     doc = parseString(xmlData)
@@ -144,7 +162,7 @@ def SearchButtonAction():
 
 def SearchLibrary():
     serverurl = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?serviceKey="
-    servervalue = "&numOfRows=999&pageNo=1&sidoName=" + urlencode(myLocationBoxData) + "&searchCondition=HOUR"
+    servervalue = "&numOfRows=999&pageSize=999&pageNo=1&startPage=1&sidoName=" + urlencode(myLocationBoxData) + "&searchCondition=HOUR"
     areaData = openAPItoXML(serverurl, myServerKey, servervalue)
     req = (getPasingData(areaData, "item"))
 
@@ -189,6 +207,28 @@ def InitSendEmailButton():
     MailButton.pack()
     MailButton.place(x=290, y=100)
 
+
+def InitFavoriteButton():
+    TempFont = font.Font(g_Tk, size=15, weight='bold', family='Consolas')
+    MailButton = Button(g_Tk, font=TempFont, text="즐겨찾기", command=SendEmailButtonAction)
+    MailButton.pack()
+    MailButton.place(x=5, y=495)
+
+
+def InitAddFavoriteLabel():
+    global EmailLabel
+    TempFont = font.Font(g_Tk, size=10, weight='bold', family='Consolas')
+    EmailLabel = Entry(g_Tk, font=TempFont, width=19, borderwidth=12, relief='ridge')
+    EmailLabel.pack()
+    EmailLabel.place(x=110, y=495)
+
+def InitAddFavoriteButton():
+    TempFont = font.Font(g_Tk, size=15, weight='bold', family='Consolas')
+    MailButton = Button(g_Tk, font=TempFont, text="즐찾추가", command=SendEmailButtonAction)
+    MailButton.pack()
+    MailButton.place(x=280, y=495)
+
+
 def SendEmailButtonAction():
     global EmailLabel
     global SearchListBox
@@ -205,14 +245,16 @@ def SendEmailButtonAction():
 
 def sendMail(ReviceMail, Subject, Content):
     s = smtplib.SMTP("smtp.gmail.com",587) #SMTP 서버 설정
+    s.ehlo()
     s.starttls() #STARTTLS 시작
-    s.login( Base64_Decode(spam.getemail()),Base64_Decode("ZWhvd2wxMjM="))
+    s.login("dohwan941024@gmail.com", "3283724aaA!")
     contents = Content
     msg = MIMEText(contents, _charset='euc-kr')
     msg['Subject'] = Subject
-    msg['From'] = Base64_Decode(spam.getemail())
+    msg['From'] = "dohwan1024@gmail.com"
     msg['To'] = ReviceMail
-    s.sendmail( Base64_Decode(spam.getemail()) , ReviceMail, msg.as_string())
+    s.sendmail( msg['To'] , ReviceMail, msg.as_string())
+    tkinter.messagebox.showinfo("결과", "이메일 전송 완료!")
 
 def Image10(sum, div):
     global g_Tk
@@ -232,6 +274,23 @@ def Image10(sum, div):
     imageLabel.configure(image = photo)
     imageLabel.image = photo
 
+def Image25(sum, div):
+    global g_Tk
+    global photo
+    avg = sum/div
+    print(avg)
+    if(0<= avg <=15):
+        photo = PhotoImage(file="0.gif")
+    elif(15< avg <= 35):
+        photo = PhotoImage(file="1.gif")
+    elif (35 < avg <= 55):
+        photo = PhotoImage(file="2.gif")
+    elif (55 < avg <= 75):
+        photo = PhotoImage(file="3.gif")
+    elif (75 < avg):
+        photo = PhotoImage(file="4.gif")
+    imageLabel2.configure(image = photo)
+    imageLabel2.image = photo
 
 InitTopText()
 #InitSearchListBox()
@@ -245,12 +304,15 @@ InitSendEmailButton()
 InitSendEmailLabel()
 #InitSortListBox()
 #InitSortButton()
-
+InitFavoriteButton()
+InitAddFavoriteLabel()
+InitAddFavoriteButton()
 #imageLabel = Label(g_Tk, image=photo)
 #imageLabel.place(x=600, y=50)
 imageLabel=Label(g_Tk,image=photo,height=70,width=70)
 imageLabel.pack(anchor='w')
+imageLabel2 = Label(g_Tk , image=photo,height = 70, width = 70)
+imageLabel2.pack(anchor='w')
 
 LoadXML()
-
 g_Tk.mainloop()
