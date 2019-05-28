b948e3655cbe0cbecfcbfd18a3765f9f8d3f3ff7
minsubkim <kpukms21@naver.com>
2019-05-28 20:45:26 +0900

    2차중간발표 수정본

--- termProject.py
+++ termProject.py
@@ -4,70 +4,134 @@ import urllib
 import xml.etree.ElementTree as ET
 from tkinter import *
 from tkinter import ttk
-from tkinter import font
+from enum import Enum
+import smtplib
+import mimetypes
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
 
-def getCount():
-    testCase = "http://openapi.seoul.go.kr:8088/5147657862716c63313231784f514f77/xml/lostArticleInfo/1/1/"
-    return int(ET.ElementTree(file=urllib.request.urlopen(testCase)).getroot().findtext('list_total_count'))
+class TextType(Enum):
+    LostItem = 0
+    LostDay = 1
+    LostSpot = 2
+    LostInfo = 3
 
-end = getCount() +1
-start = end - 50
-mylists = []
+class MyTk:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title('분실물 찾기 서비스')
+        self.root.geometry('600x800')
+        self.mylist = Listbox(self.root, selectmode='extended')
+        self.mylist.place(x=20, y=50, width=200, height=400)
+        self.strings = StringVar()
+        self.emailadd = StringVar()
+        self.textbox = ttk.Entry(self.root, textvariable=self.strings)
+        self.textbox.place(x=20, y=20, width=200)
 
-def myPrint():
-    global start
-    global end
-    global mylists
-    global mylist
-    yIdx = 0
-    url = "http://openapi.seoul.go.kr:8088/5147657862716c63313231784f514f77/xml/lostArticleInfo/" + str(
-        start) + "/" + str(end) + "/"
+        self.textbox2 = ttk.Entry(self.root, textvariable = self.emailadd)
+        self.textbox2.place(x=20, y =470, width = 400)
 
-    tree = ET.ElementTree(file=urllib.request.urlopen(url))
-    root = tree.getroot()
-    for a in root.findall('row'):
-        if len(mylists) <= yIdx:
-            mylists.append([])
+        self.searchButton = Button(self.root, text ="검색", overrelief="solid", command=self.getList, repeatdelay=1000, repeatinterval=100)
+        self.searchButton.place(x=230, y=20, width=50, height=20)
 
-        mylists[yIdx].append(a.findtext('GET_NAME'))
-        mylists[yIdx].append(a.findtext('REG_DATE'))
-        yIdx += 1
-    for i in range(0,len(mylists)-1,1):
-        mylist.insert(i, mylists[i])
+        self.sendButton = Button(self.root, text = "전송", overrelief = "solid", command=self.sendButtonAction)
+        self.sendButton.place(x =480, y = 470, width = 50, height =20)
 
-    print(mylists)
+        self.Label1 = Label(self.root, text="", relief='solid')
+        self.Label1.place(x=250, y=80, width=300, height=40)
 
-root = Tk()
-root.title('분실물 찾기 서비스')
-root.geometry('600x800')
-mylist = Listbox(root, selectmode = 'extended')
-mylist.place(x= 20, y = 50, width = 200, height = 400)
+        self.Label2 = Label(self.root, text="", relief='solid')
+        self.Label2.place(x=250, y=150, width=300, height=40)
 
-strings = ""
-textbox = ttk.Entry(root, textvariable = strings)
-textbox.place(x = 20, y = 20, width = 200)
+        self.Label3 = Label(self.root, text="", relief='solid')
+        self.Label3.place(x=250, y=220, width=300, height=40)
 
-searchButton = Button(root, text="검색", overrelief = "solid", command = myPrint, repeatdelay = 1000, repeatinterval = 100)
-searchButton.place(x = 220, y = 20, width = 50, height = 20)
+        self.Label4 = Label(self.root, text="", relief='solid', wraplength = 300)
+        self.Label4.place(x=250, y=300, width=300, height=150)
+
+        self.Label5 = Label(self.root, text="", relief='solid')
+        self.Label5.place(x=20, y=500, width=560, height=250)
+
+        self.mylist.bind("<Double-Button-1>", self.selection)
+
+        self.end = self.getCount()
+        self.start = self.end - 201
+        self.mylists = []
+        self.yIdx = 0
+        self.plan = 10
+        self.selectNum = 0
+        staticText = Label(self.root, text="분실 날짜")
+        staticText.place(x = 360, y = 60, width = 80, height = 20)
+
+        staticText2 = Label(self.root, text="분실 물건")
+        staticText2.place(x=360, y=130, width=80, height=20)
+
+        staticText3 = Label(self.root, text="분실 장소")
+        staticText3.place(x=360, y=200, width=80, height=20)
+
+        staticText4 = Label(self.root, text="분실 정보")
+        staticText4.place(x=360, y=280, width=80, height=20)
+
+        staticText5 = Label(self.root, text = "이메일 전송")
+        staticText5.place(x=20, y = 450, width=80, height=20)
+
+        self.s = smtplib.SMTP('smtp.gmail.com',587)
+        self.s.starttls()
+        self.s.login('dohwan941024@gmail.com', 'dlugdtnyapgdkwqp')
+        self.msg = MIMEText('내용: 본문 내용 테스트')
+        self.msg['Subject'] = '제목: 메일 보내기 테스트'
 
 
-Label1 = Label(root, text="분실 일자:", relief = 'solid')
-Label1.place( x = 250, y = 80, width = 300, height = 40)
+        self.root.mainloop()
 
-Label2 = Label(root, text="분실 품명:", relief = 'solid')
-Label2.place( x = 250, y = 150, width = 300, height = 40)
+    def sendButtonAction(self):
+        receive = self.textbox2.get()
+        listargs = self.mylists[self.selectNum]
 
-Label3 = Label(root, text="분실 장소:", relief = 'solid')
-Label3.place( x = 250, y = 220, width = 300, height = 40)
+        msg = MIMEText('분실 날짜 : ' + listargs[TextType.LostDay.value] + '\n' + '분실 물건 :  ' + listargs[TextType.LostItem.value] + '\n' + '분실 장소 : ' + listargs[TextType.LostSpot.value] + '\n' + '분실 정보 : \n' + listargs[TextType.LostInfo.value])
+        msg['Subject'] = "분실물 정보에 대한 이메일입니다."
 
-Label4 = Label(root, text="분실 정보", relief = 'solid')
-Label4.place( x = 250, y = 300, width = 300, height = 150)
+        self.s.sendmail("dohwan941024@gmail.com", receive,msg.as_string())
 
-Label5 = Label(root, text="이미지", relief = 'solid')
-Label5.place( x = 20, y = 500, width = 560, height = 250)
+        self.s.quit()
 
-TempFont = font.Font(root, size=18, weight='bold', family='Consolas')
-MainText = Label(root, font=TempFont, text="[분실물 찾기 서비스]")
-MainText.place(x=350)
+    def selection(self, event):
+        listargs = self.mylists[event.widget.curselection()[0]]
+        self.selectNum = event.widget.curselection()[0]
+        self.Label1.config(text = listargs[TextType.LostDay.value])
+        self.Label2.config(text = listargs[TextType.LostItem.value])
+        self.Label3.config(text=listargs[TextType.LostSpot.value])
+        self.Label4.config(text=listargs[TextType.LostInfo.value])
 
-root.mainloop()
+    def getCount(self):
+        testCase = "http://openapi.seoul.go.kr:8088/5147657862716c63313231784f514f77/xml/lostArticleInfo/1/1/"
+        return int(ET.ElementTree(file=urllib.request.urlopen(testCase)).getroot().findtext('list_total_count'))
+    def getList(self):
+        url = "http://openapi.seoul.go.kr:8088/5147657862716c63313231784f514f77/xml/lostArticleInfo/" + str(
+            self.start) + "/" + str(self.end) + "/"
+        print(self.strings.get())
+        tree = ET.ElementTree(file=urllib.request.urlopen(url))
+        root = tree.getroot()
+        for a in root.findall('row'):
+            condition = a.findtext('GET_NAME')
+            if a.findtext('STATUS') == "수령" or condition.find("고객") != -1 or condition.find("연락") != -1 or condition.find(self.strings.get()) == -1:
+                continue
+
+            if len(self.mylists) <= self.yIdx:
+                self.mylists.append([])
+
+            self.mylists[self.yIdx].append(a.findtext('GET_NAME'))
+            self.mylists[self.yIdx].append(a.findtext('REG_DATE'))
+            self.mylists[self.yIdx].append(a.findtext('GET_GOOD'))
+            self.mylists[self.yIdx].append(a.findtext('GET_THING').replace("<br>", "\n"))
+
+            self.mylist.insert(self.yIdx, self.mylists[self.yIdx][TextType.LostItem.value])
+
+            self.yIdx += 1
+        self.end = self.start
+        self.start = self.end - 201
+        self.plan += 10
+
+
+ab = MyTk()
+
